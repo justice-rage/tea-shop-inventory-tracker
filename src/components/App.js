75c160b11af7b8649cf02c75ef7757ee8d2b639e
justicepelteir <justice.douglas@hey.com>
2022-08-13 13:12:46 -0700

    slim down code with imports

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TeaList from "./TeaList"
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <TeaList />
+    </React.Fragment>
+  );
+}
+
+export default App;
