b3ec73fe3a44327dd667c7506c86e1ea28432303
justicepelteir <justice.douglas@hey.com>
2022-08-13 13:45:38 -0700

    add PropType and strict data type declaration

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -1,14 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Tea(props){
   return (
     <React.Fragment>
-      <h3>{props.name} - {props.origin}</h3>
+      <h3>{props.names} - {props.origin}</h3>
       <h3>{props.price}</h3>
       <p><em></em></p>
       <hr/>
     </React.Fragment>
   );
 }
+
+Tea.propTypes = {
+    names: PropTypes.string,
+    origin: PropTypes.string,
+    price: PropTypes.number
+};
 
 export default Tea;
