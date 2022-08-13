292e5734edce53432c65c5f8ea5035c4d405fb52
justicepelteir <justice.douglas@hey.com>
2022-08-13 14:44:55 -0700

    add array of tea

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Tea from "./Tea";
 
+const mainTeaList = [
+    {
+      names: 'Green',
+      origin: 'Vietnam',
+      price: 1
+    },
+    {
+      names: 'Black',
+      origin: 'England',
+      price: 2
+    },
+    {
+      names: 'Matcha',
+      origin: 'Japan',
+      price: 3
+    }
+  ];  
+
 function TeaList(){
   return (
       <React.Fragment>
-        <Tea 
-            names="Green"
-            origin="Vietnam"
-            price={1} />
-        <Tea 
-            names="Black"
-            origin="England"
-            price={2} />
+        {mainTeaList.map((tea, index) =>
+          <Tea names={tea.names}
+            origin={tea.origin}
+            price={tea.price}
+            key={index}/>
+        )}
       </React.Fragment>
   );
 }
